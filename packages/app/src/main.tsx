1c088a87844b984607459415293a67f31e3d6a93
Hannah <hannah@harubex.com>
2023-08-20 15:50:05 -0600

    Adding manifest to html

--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -3,8 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
 
+const addManifest = () => {
+    const manifestLink = document.createElement("link");
+    manifestLink.rel = "manifest";
+    manifestLink.href = "/manifest.webmanifest";
+    document.head.appendChild(manifestLink);
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+addManifest();
