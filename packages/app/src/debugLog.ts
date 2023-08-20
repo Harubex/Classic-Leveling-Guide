73f6c1555e842f01c6a9cc06fd4395adab411416
Hannah <hannah@harubex.com>
2023-08-20 13:49:32 -0600

    Docs

--- packages/app/src/debugLog.ts
+++ packages/app/src/debugLog.ts
@@ -1,3 +1,7 @@
+/**
+ * Logs to the browser console when developing this app locally.
+ * @param data The stuff to print out.
+ */
 export const debug = (...data: any[]) => {
     if (import.meta.env.MODE === "development") {
         console.log(data);
